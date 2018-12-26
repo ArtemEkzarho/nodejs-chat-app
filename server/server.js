04272f921da10f1ef13dc3fd32c18f9973d9950c
Artem Ekzarho <artem.ekzarho@gmail.com>
2018-12-26 17:30:06 +0200

    Emit newMessage event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 require('./config/config');
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected'); 
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+           from: message.from,
+           text: message.text,
+           createdAt: new Date().getTime()
+        })
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconected');
+    });
+});
+
+server.listen(process.env.PORT, () => {
     console.log(`Started on port ${process.env.PORT}`);
 });
