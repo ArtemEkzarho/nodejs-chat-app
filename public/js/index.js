0cda31289478df1f8bfa40eee53b2b3961ffa02a
Artem Ekzarho <artem.ekzarho@gmail.com>
2018-12-29 19:27:26 +0200

    add templates and autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+var locationBtn = $('#send-location');
+var messageForm = $('#message-form');
+
+function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = $('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -10,24 +28,32 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var foramtedDate = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${foramtedDate}: ${message.text}`);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: foramtedDate
+    })
 
-    $('#messages').append(li);
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
     var foramtedDate = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target=_blank>My current location</a>');
+    var template = $('#message-location-template').html();
 
-    li.text(`${message.from} ${foramtedDate}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: foramtedDate
+    })
+
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
-$('#message-form').on('submit', function (e) {
+messageForm.on('submit', function (e) {
     e.preventDefault();
     
     var input = $('[name=message]');
@@ -39,8 +65,6 @@ $('#message-form').on('submit', function (e) {
         input.val('');
     });
 });
-
-var locationBtn = $('#send-location');
 
 locationBtn.on('click', function () {
     if (!navigator.geolocation) {
